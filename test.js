353cc6805ff4a6853e3ca074ad1b954de6585b72
Tzipi <TzipiH@skyvar.co.il>
2024-09-24 13:20:51 +0300

    try

---
+++ test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+function add(a, b) {
+  return a + b;
+}
+
+describe('Add function', function() {
+  it('should return the sum of two numbers', function() {
+    assert.strictEqual(add(1, 2), 3);
+    console.log("Hello world!!!");
+  });
+});
